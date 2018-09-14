63d6d640630edc754e808f69b3ba90fb99b13acf
Hamza Muzaffar :) <hamza.codingpixel@gmail.com>
2018-09-14 09:16:56 +0500

    push

---
+++ database/migrations/2018_09_14_014328_create_artist_databse.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtistDatabse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+
+            $table->string('id', 30);
+            $table->string('name', 30);
+            $table->unsignedInteger('followers');
+            $table->unsignedDecimal('popularity');
+            $table->primary('id');
+
+
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('artists');
+    }
+}
