a8162818b2d8b52baaf1f0a3c92836f59cbfae37
Hamza Muzaffar <hamza.codingpixel@gmail.com>
2018-11-05 17:31:44 +0500

    temp

--- app/playlist_other_tag.php
+++ app/playlist_other_tag.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Playlist_Other_tag extends Model
 {
     public $timestamps = False;
+    public $table = "playlist_other_tags";
     public $incrementing = false;
     public $primaryKey = ['other_tag_id', 'playlist_id'];
 
