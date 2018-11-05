c5484f2502c272fced3ca2661d40a6b3b623aa27
Hamza Muzaffar <hamza.codingpixel@gmail.com>
2018-11-05 17:16:51 +0500

    temp

--- app/playlist_other_tag.php
+++ app/playlist_other_tag.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class playlist_other_tag extends Model
+class Playlist_Other_tag extends Model
 {
     public $timestamps = False;
     public $incrementing = false;
