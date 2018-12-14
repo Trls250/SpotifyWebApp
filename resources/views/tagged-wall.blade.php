a9a74857f72bc0eb25de67e9d0cdc59e9c99c665
Coding Pixel <myemail@example.com>
2018-12-14 16:06:45 +0500

    fixed literal thing

--- resources/views/tagged-wall.blade.php
+++ resources/views/tagged-wall.blade.php
@@ -71,7 +71,7 @@
                             console.log("flag:" + flag);
                         }
                     }
-                }`
+                }
             });
 
           function getRecords() {
